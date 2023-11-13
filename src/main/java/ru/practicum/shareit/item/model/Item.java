861b41ad916443d281c3555aa6d50afc894288df
Андрей Ходюкевич <Lolds456@yandex.ru>
2023-11-13 20:17:52 +0400

    Sprint 14 version 1.0

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -16,8 +16,11 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "is_available")
     private boolean available;
     @ManyToOne
     @JoinColumn(name = "owner_id")
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -15,6 +15,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "name")
     private String name;
     @Column(unique = true)
     private String email;
